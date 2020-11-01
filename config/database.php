b37d3f902dd4382d93c82fa2ee3105eab9b11423
Firdha10 <firdhaelma@gmail.com>
2020-11-01 23:21:38 +0800

    commit

--- config/database.php
+++ config/database.php
@@ -46,7 +46,6 @@ return [
             'database' => 'pustaka',
             'username' => 'siperpus',
             'password' => 'almari10',
-            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
